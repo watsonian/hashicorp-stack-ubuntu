0348b65f5732a319f5041b26deef71f85bab83da
Joel Watson <joel@watsonian.net>
2021-09-21 12:00:51 -0500

    Wire Nomad up to Vault properly

--- scripts/bootstrap-nomad.sh
+++ scripts/bootstrap-nomad.sh
@@ -8,5 +8,26 @@ if [ -f /vagrant/config/nomad-${ROLE}-host-volumes.hcl ]; then
     sudo ln -sf /vagrant/config/nomad-${ROLE}-host-volumes.hcl /etc/nomad.d/host-volumes.hcl
 fi
 
+token=$(jq -r '.auth.client_token' /vagrant/nomad-server-vault-token.json)
+
+# drop the Vault configuration file
+if [ "$ROLE" = "client" ]; then
+    cat <<-EOF >> /etc/nomad.d/vault.hcl
+vault {
+  enabled          = true
+  address          = "http://vault.service.consul:8200"
+}
+EOF
+else
+    cat <<-EOF >> /etc/nomad.d/vault.hcl
+vault {
+  enabled          = true
+  address          = "http://vault.service.consul:8200"
+  token            = "${token}"
+  create_from_role = "nomad-cluster"
+}
+EOF
+fi
+
 sudo systemctl enable nomad.service
 sudo systemctl start nomad
--- scripts/bootstrap-vault.sh
+++ scripts/bootstrap-vault.sh
@@ -33,7 +33,39 @@ export VAULT_ADDR="http://127.0.0.1:8200"
 vault operator init -key-shares=1 -key-threshold=1 -format=json > /vagrant/vault-keys.json
 vault operator unseal $(jq -r '.unseal_keys_hex[0]' /vagrant/vault-keys.json)
 echo
+
+root_token=$(jq -r '.root_token' /vagrant/vault-keys.json)
+export VAULT_TOKEN="$root_token"
+
 echo "Your Vault root token is:"
 echo
-jq -r '.root_token' /vagrant/vault-keys.json
+echo "$root_token"
 echo
+
+# wait for Vault to be healthy
+echo "Waiting for Vault to be healthy..."
+resp_code=""
+until [ "${resp_code}" = "200" ]; do
+  sleep 1
+  resp_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8200/v1/sys/health)
+done
+
+#
+# Create Nomad policy, role, and token for Nomad
+#
+
+# Download the policy and token role
+curl https://nomadproject.io/data/vault/nomad-server-policy.hcl -O -s -L
+curl https://nomadproject.io/data/vault/nomad-cluster-role.json -O -s -L
+
+# Write the policy to Vault
+vault policy write nomad-server nomad-server-policy.hcl
+
+# Create the token role with Vault
+vault write /auth/token/roles/nomad-cluster @nomad-cluster-role.json
+
+# Create the Vault token based on the token role
+vault token create -policy nomad-server -period 72h -orphan -format=json > /vagrant/nomad-server-vault-token.json
+
+# Enable kv v2 secrets engine
+vault secrets enable -version=2 kv
